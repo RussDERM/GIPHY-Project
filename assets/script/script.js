31cdefc3053b35435e18aada8a2d87937f51e924
Russ <russ.dermody@gmail.com>
2019-04-04 02:30:21 -0700

    mostly working, need to update in the morning

--- assets/script/script.js
+++ assets/script/script.js
@@ -9,38 +9,65 @@
 // select submit button, attach click event
 $('#submit-button').on('click', function () {
 
+    //declare array that will hold all img URLs
     var imgURL = [];
+    //holds img ratings
+    var imgRating = [];
+    var img;
+    var rating;
+    var query
+
+    //stores value from text box as query 
+    $('submit-button').on('click', function () {
+        //clear any existing pictures
+        $('#gif-container').html();
+        query = $('#input').val();
+        $('#input').html();
+
+    });
+
 
     //variable to set queryURL
-    var queryURL = 'https://api.giphy.com/v1/gifs/search?q=explosion&api_key=csKwWJmlyqvvCH9Z5iDDevJnvgIvylmn&limit=10'
+    var queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + query + '&api_key=csKwWJmlyqvvCH9Z5iDDevJnvgIvylmn&limit=10'
     //ajax call
     $.ajax({
         url: queryURL,
         method: 'GET'
     }).then(function (response) {
+        //grab response and shove it in imgData
         var imgData = response.data;
+        //cycle through the responses, and push data into variable
         for (let i = 0; i < 10; i++) {
+            //embed URL
+            imgURL.push(response.data[i].images.fixed_height_still.url);
+            //Rating
+            imgRating.push(imgData[i].rating);
 
-            imgURL = imgData[3].url;
+            //empty html elements to stick stuff in
+            var img = $('<img>').attr('src', imgURL[i]);
+            var rating = $('<p>').text('rated : ' + imgRating[i]);
+            img.append(rating);
+            $('#gif-container').append(img);
+
+
+
+
+            console.log(img);
+            //rating.attr('text', imgRating[i]);
+            //attach rating to gif
+            //img.prepend(rating)
+            //attach gif with rating to img
+            //$('gif-container').prepend(img);
+
         }
-        //var imgURL = imgData[i];
+
+
+        console.log(response)
         console.log(imgData);
         console.log(imgURL);
-
-
-        // console.log(imgURL);
-
+        console.log(imgRating);
     });
-    //console.log(imgData);
-    console.log()
-    //need a loop to cycle through the response and grab what I need
-    // for (let i = 0; i < 9; i++) {
-
-    //     var imgURL = imgData.data.[i]
-
-
-    // }
-    //variables, to store rating and img url
+    //Now, to generate jQuery objects with the data I grabbed
 
 
 
